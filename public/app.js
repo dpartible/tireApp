a81c1f1f96ae96ebcd203b8377adfc947fcdf3b2
Derek Partible <dpartible92@gmail.com>
2016-11-17 20:29:57 -0800

    Changed placeholder text for search

--- public/app.js
+++ public/app.js
@@ -7,56 +7,58 @@ var myApp = angular.module('myApp', ['ui.router', 'myAppCtrl']);
 
 myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
 
+	console.log('running my app config');
+
 	$urlRouterProvider.otherwise('/');	// if url is unknown default to home page
-	
+
 	$stateProvider
 		.state('home', {	// set home page to vehicle search
 
 			url: '/',
 			views:{
 				"mainBody": {
-					templateUrl: 'partials/byvehicle.html', 
+					templateUrl: 'partials/byvehicle.html',
 					controller: 'MyAppController'
 				},"dataTable": {
-					templateUrl: 'partials/dataTable.html', 
+					templateUrl: 'partials/dataTable.html',
 					controller: 'MyAppController'
 				},
 				"navBar": {
-					templateUrl: 'partials/navBar.html', 
+					templateUrl: 'partials/navBar.html',
 					controller: 'MyAppController'
 				}
 			}
-			
+
 
 		}).state('byvehicle', {	// set route for search by vehicle
 
 			url: '/byvehicle',
 			views:{
 				"mainBody": {
-					templateUrl: 'partials/byvehicle.html', 
+					templateUrl: 'partials/byvehicle.html',
 					controller: 'MyAppController'
 				},
 				"dataTable": {
-					templateUrl: 'partials/dataTable.html', 
+					templateUrl: 'partials/dataTable.html',
 					controller: 'MyAppController'
 				},
 				"navBar": {
-					templateUrl: 'partials/navBar.html', 
+					templateUrl: 'partials/navBar.html',
 					controller: 'MyAppController'
 				}
 			}
-			
+
 
 		}).state('bytiresize', {	// set route for search by tire size
 
 			url: '/bysize',
 			views:{
 				"mainBody": {
-					templateUrl: 'partials/bytiresize.html', 
+					templateUrl: 'partials/bytiresize.html',
 					controller: 'MyAppController'
 				},
 				"navBar": {
-					templateUrl: 'partials/navBar.html', 
+					templateUrl: 'partials/navBar.html',
 					controller: 'MyAppController'
 				}
 			}
